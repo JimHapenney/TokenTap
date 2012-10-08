b7dea5338b0aab56c7ed363b3c4498fd189a5f43
Jim Hapenney <jimhap@gmail.com>
2012-10-08 00:54:25 -0500

    test

--- index.js
+++ index.js
@@ -518,9 +518,9 @@ $(document).bind("mobileinit", function () {
 	$.mobile.page.prototype.options.backBtnTheme = "b";
 
 	// Page
-	$.mobile.page.prototype.options.theme = "d";
+	$.mobile.page.prototype.options.theme = "b";
 	$.mobile.page.prototype.options.headerTheme = "b";
-	$.mobile.page.prototype.options.contentTheme = "d";
+	$.mobile.page.prototype.options.contentTheme = "b";
 	$.mobile.page.prototype.options.footerTheme = "b";
 
 	// Controls
@@ -540,6 +540,72 @@ $(document).bind("mobileinit", function () {
 	$(document).ready(function () { Application.ready(); });
 
 });
+
+
+
+var app = {
+    // denotes whether we are within a mobile device (otherwise we're in a browser)
+    iAmPhoneGap: false,
+    // how long should we wait for PhoneGap to say the device is ready.
+    howPatientAreWe: 1000,
+    // id of the 'too_impatient' timeout
+    timeoutID: null,
+    // id of the 'impatience_remaining' interval reporting.
+    impatienceProgressIntervalID: null,
+
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // `load`, `deviceready`, `offline`, and `online`.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        // after 10 seconds, if we still think we're NOT phonegap, give up.
+        app.timeoutID = window.setTimeout(function(appReference) {
+            if (!app.iAmPhoneGap) // jeepers, this has taken too long.
+                // manually trigger (fudge) the receivedEvent() method.   
+                appReference.receivedEvent('too_impatient');
+        }, app.howPatientAreWe, this);
+        // keep us updated on the console about how much longer to wait.
+        app.impatienceProgressIntervalID = window.setInterval(function areWeThereYet() {
+                if (typeof areWeThereYet.howLongLeft == "undefined") { 
+                    areWeThereYet.howLongLeft = app.howPatientAreWe; // create a static variable
+                } 
+                areWeThereYet.howLongLeft -= 100; // not so much longer to wait.
+
+                console.log("areWeThereYet: Will give PhoneGap another " + areWeThereYet.howLongLeft + "ms");
+            }, 1000);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of `this` is the event. In order to call the `receivedEvent`
+    // function, we must explicity call `app.receivedEvent(...);`
+    onDeviceReady: function() {
+        app.iAmPhoneGap = true; // We have a device.
+        app.receivedEvent('deviceready');
+
+        // clear the 'too_impatient' timeout .
+        window.clearTimeout(app.timeoutID); 
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        // clear the "areWeThereYet" reporting.
+        window.clearInterval(app.impatienceProgressIntervalID);
+        console.log('Received Event: ' + id);
+        myCustomJS(app.iAmPhoneGap); // run my application.
+    }
+};
+
+app.initialize();
+
+function myCustomJS(trueIfIAmPhoneGap) {
+    // put your custom javascript here.
+    alert("I am "+ (trueIfIAmPhoneGap?"PhoneGap":"a Browser"));
+}
+
 
 
 
@@ -609,7 +675,7 @@ Dashboard = {
 		});
 
 		$("#GoAllParties", jPage).onpress(function () {
-			Participants.oClass = $.extend({}, {
+			Participants.oClass = $.extend(true, {}, {
 				CLASS_NAME: "All " + Application.oUser.PARTICIPANT_ALIAS + "s",
 				ID: "-1"
 			}, Dashboard.oTotals);
